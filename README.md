1c3cb162377ab199bf8dbd1c5a897d8abb224931
inthej <inthej12@gmail.com>
2022-02-02 17:49:55 +0900

    Update Example, README.md and document.txt

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # vanilla-redux-sample
-[리액트를 다루는 기술](http://www.kyobobook.co.kr/product/detailViewKor.laf?mallGb=KOR&ejkGb=KOR&barcode=9791160508796) 책 예제입니다
+[리액트를 다루는 기술][1] 책 예제입니다
 
-### INSTALL
+### Install
 ```
 $ yarn global add parcel-bundler
 $ yarn init -y
 $ yarn add redux
 ```
+
+### Reference
+- 번들러: [https://pimpdevelop.tistory.com/9][2]
+- 순수함수: [https://velog.io/@chdb57/%E3%85%87%E3%85%87%E3%85%87%E3%85%87%E3%85%87%E3%85%87%E3%85%87%E3%85%87][3] 
+
+[1]: http://www.kyobobook.co.kr/product/detailViewKor.laf?mallGb=KOR&ejkGb=KOR&barcode=9791160508796
+[2]: https://pimpdevelop.tistory.com/9
+[3]: https://velog.io/@chdb57/%E3%85%87%E3%85%87%E3%85%87%E3%85%87%E3%85%87%E3%85%87%E3%85%87%E3%85%87
--- src/index.js
+++ src/index.js
@@ -1,1 +1,94 @@
-console.log('hello world!')
+// DOM 레퍼런스
+import { createStore } from "redux";
+
+const divToggle = document.querySelector('.toggle')
+const counter = document.querySelector('h1')
+const increaseBtn = document.querySelector('#increase')
+const decreaseBtn = document.querySelector('#decrease')
+
+// Action 타입
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH'
+const INCREASE = 'INCREASE'
+const DECREASE = 'DECREASE'
+
+// Action 생성함수 (type 값 필수)
+const toggleSwitch = () => {
+  return {
+    type: TOGGLE_SWITCH
+  }
+}
+
+const increase = difference => {
+  return {
+    type: INCREASE,
+    difference
+  }
+}
+
+const decrease = () => {
+  return {
+    type: DECREASE
+  }
+}
+
+// 초기값 설정
+const initialState = {
+  toggle: false,
+  count: 0
+}
+
+// 리듀서 함수 (변화를 일으키는 함수 // 불변성을 유지 // 순수함수)
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle
+      }
+    case INCREASE:
+      return {
+        ...state,
+        count: state.count + action.difference
+      }
+    case DECREASE:
+      return {
+        ...state,
+        count: state.count - 1
+      }
+    default:
+      return state
+  }
+}
+
+// 스토어 생성
+const store = createStore(reducer)
+
+// Render
+const render = () => {
+  const state = store.getState() // 현재상태를 불러온다
+  if (state.toggle) {
+    divToggle.classList.add('active')
+  } else {
+    divToggle.classList.remove('active')
+  }
+
+  counter.innerText = state.count
+}
+
+render()
+
+// 구독하기 - 스토어 상태가 바뀔때마다 render 함수가 실행되도록 한다
+store.subscribe(render)
+
+// 디스패치 - 액셜 발생시키기
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch())
+}
+
+increaseBtn.onclick = () => {
+  store.dispatch(increase(1))
+}
+
+decreaseBtn.onclick = () => {
+  store.dispatch(decrease())
+}
